4110b92bfe2ed94625fb3a7e020cdca244c68c8e
Code_Junior <46490572+essienmichael4@users.noreply.github.com>
2021-11-02 09:35:12 +0000

    new

--- main.js
+++ main.js
@@ -6,22 +6,30 @@ let services = document.querySelector(".services")
 let locations = document.querySelector(".locations")
 let legal = document.querySelector(".legal")
 let imgcontainer = document.querySelector(".imgcontainer")
+let serviceCTA = document.querySelector(".callToActionServices")
+let locationCTA = document.querySelector(".callToActionLocation")
+let ctaService = document.querySelector("#ctaService")
+let ctaLocation = document.querySelector("#ctaLocation")
 
 let menuIsClicked = false;
-
-console.log(menu.style.width)
 
 const openmenu = ()=>{
     if(menuIsClicked == true){
         menu.style.width = "6rem";
-        // content.style.visibility = "hidden"; 
         text.innerText = "menu";
         icon.classList.remove('fa-times')
         icon.classList.add('fa-bars')
         menuIsClicked = false;
     }else if(menuIsClicked == false){
+        if(serviceCTA.classList.contains("ctaactive")){
+            serviceCTA.classList.remove("ctaactive")
+        }
+
+        if(locationCTA.classList.contains("ctaactive")){
+            locationCTA.classList.remove("ctaactive")
+        }
+
         menu.style.width = "100%";
-        // content.style.visibility = "visible";
         text.innerText = "close";
         icon.classList.remove('fa-bars')
         icon.classList.add('fa-times')
@@ -43,3 +51,26 @@ legal.addEventListener("onmouseover", ()=>{
 const picChange = (e)=>{
     imgcontainer.style.backgroundImage = `url(${e})`;
 }
+
+
+ctaService.addEventListener("click", ()=>{
+    if(serviceCTA.classList.contains("ctaactive")){
+        serviceCTA.classList.remove("ctaactive")
+    }else{
+        if(locationCTA.classList.contains("ctaactive")){
+            locationCTA.classList.remove("ctaactive")
+        }
+        serviceCTA.classList.add("ctaactive")
+    }
+})
+
+ctaLocation.addEventListener("click", ()=>{
+    if(locationCTA.classList.contains("ctaactive")){
+        locationCTA.classList.remove("ctaactive")
+    }else{
+        if(serviceCTA.classList.contains("ctaactive")){
+            serviceCTA.classList.remove("ctaactive")
+        }
+        locationCTA.classList.add("ctaactive")
+    }
+})
